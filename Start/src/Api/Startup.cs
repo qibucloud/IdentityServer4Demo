a3df27345f71fefb77a22a87f92004519cabbdc8
Dominick Baier <dbaier@leastprivilege.com>
2016-09-21 13:42:47 +0200

    initial commit

---
+++ Start/src/Api/Startup.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Api
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvcCore()
+                .AddJsonFormatters()
+                .AddAuthorization();
+        }
+
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(LogLevel.Trace);
+            app.UseDeveloperExceptionPage();
+
+            app.UseMvc();
+        }
+    }
+}
---
+++ Start/src/IdentityServer4Demo/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace IdentityServer4Demo
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(LogLevel.Debug);
+            app.UseDeveloperExceptionPage();
+        }
+    }
+}
